c6db3966453baa26fb2817e61b9bdce42dbcc0a7
jcg678 <jcg678@hotmail.es>
2019-01-24 20:22:14 +0100

    caja principal

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 	<body>
 		<!--CABECERA-->
-		<header>
+		<header id="cabecera">
 			<!--LOGO-->
 			<div id="logo">
 				<a href="index.php">
@@ -15,7 +15,7 @@
 				</a>
 			</div>
 			<!--MENU-->
-			<nav id="nav">
+			<nav id="menu">
 				<ul>
 					<li>
 						<a href="index.php">Inicio</a>
@@ -39,10 +39,66 @@
 						<a href="index.php">Contacto</a>
 					</li>
 				</ul>	
-
-				
 			</nav>	
-		</header>	
+		</header>
+		<div id="contenedor">
+			<!--BARRA LATERAL -->
+			<aside id="sidebar">
+				<div id="login" class="bloque">
+					<h3>Identificate</h3>
+					<form action="login.php" method="POST">
+						<label for="email">Email</label>
+						<input type="email" name="email">
+						<label for="password">Password</label>
+						<input type="password" name="password">
+						<input type="submit" name="Entrar">		
+					</form>
+				</div>
+
+				<div id="register" class="bloque">
+					<h3>Registrate</h3>
+					<form action="registro.php" method="POST">
+						<label for="nombre">Nombre</label>
+						<input type="text" name="nombre">						
+						<label for="apellidos">Apellidos</label>
+						<input type="text" name="apellidos">
+						<label for="email">Email</label>
+						<input type="email" name="email">
+						<label for="password">Password</label>
+						<input type="password" name="password">
+						<input type="submit" name="Registrar">		
+					</form>
+				</div>
+			</aside>
+			<!--CAJA PRINCIPAL-->
+			<div id="principal">
+				<h1>Últimas Entradas</h1>
+				<article class="entrada">
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+					</p>
+				</article>
+				<article class="entrada">
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+					</p>
+				</article>
+				<article class="entrada">
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+					</p>
+				</article>
+				<article class="entrada">
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+					</p>
+				</article>												
+			</div>	
+		</div>	
 
 		<!--BARRA LATERAL-->
 		<!--CAJA PRINCIPAL-->
