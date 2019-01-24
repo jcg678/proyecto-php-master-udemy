c44143dc5d5b73a6b73c39b3cbeb0019773d93bf
jcg678 <jcg678@hotmail.es>
2019-01-24 19:43:22 +0100

    menu inicial

--- index.php
+++ index.php
@@ -8,13 +8,42 @@
 	<body>
 		<!--CABECERA-->
 		<header>
+			<!--LOGO-->
 			<div id="logo">
 				<a href="index.php">
 					Blog de Videojuegos	
 				</a>
-			</div>	
+			</div>
+			<!--MENU-->
+			<nav id="nav">
+				<ul>
+					<li>
+						<a href="index.php">Inicio</a>
+					</li>
+						<li>
+						<a href="index.php">Inicio</a>
+					</li>
+						<li>
+						<a href="index.php">Inicio</a>
+					</li>
+						<li>
+						<a href="index.php">Inicio</a>
+					</li>
+						<li>
+						<a href="index.php">Inicio</a>
+					</li>
+					<li>
+						<a href="index.php">Sobre mí</a>
+					</li>
+					<li>
+						<a href="index.php">Contacto</a>
+					</li>
+				</ul>	
+
+				
+			</nav>	
 		</header>	
-		<!--MENU-->
+
 		<!--BARRA LATERAL-->
 		<!--CAJA PRINCIPAL-->
 		<!--PIE DE PAGINA-->
