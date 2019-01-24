ce0f32ad7c3e58c801b66a239315ff704a36721b
jcg678 <jcg678@hotmail.es>
2019-01-24 19:35:27 +0100

    recursos iniciales mas index

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Blog de Videjuegos</title>
+	</head>
+	<body>
+		<!--CABECERA-->
+		<header>
+			<div id="logo">
+				<a href="index.php">
+					Blog de Videojuegos	
+				</a>
+			</div>	
+		</header>	
+		<!--MENU-->
+		<!--BARRA LATERAL-->
+		<!--CAJA PRINCIPAL-->
+		<!--PIE DE PAGINA-->
+	</body>
+
+</html>
