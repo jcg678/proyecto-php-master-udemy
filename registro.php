23f6ccccdd02134a5bf132c95fb03192a77a645e
jcg678 <jcg678@hotmail.es>
2019-01-30 21:26:40 +0100

    escapar los datos

--- registro.php
+++ registro.php
@@ -3,12 +3,16 @@
 if(isset($_POST)){
 	require_once 'includes/conexion.php';
 
-	//session_start();	
+	//session_start();
+	if (!isset($_SESSION)) {
+		session_start();
+	}
+
 	//Recoger los valores del formulario
-	$nombre = isset($_POST['nombre'])? $_POST['nombre'] : false;
-	$apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
-	$email = isset($_POST['email']) ? $_POST['email'] : false;
-	$password = isset($_POST['password']) ? $_POST['password'] : false ;
+	$nombre = isset($_POST['nombre'])? mysqli_real_escape_string($db , $_POST['nombre']) : false;
+	$apellidos = isset($_POST['apellidos']) ? mysqli_real_escape_string($db , $_POST['apellidos']) : false;
+	$email = isset($_POST['email']) ? mysqli_real_escape_string($db , $_POST['email']) : false;
+	$password = isset($_POST['password']) ? mysqli_real_escape_string($db , $_POST['password']) : false ;
 
 	$errores = [];
 	//Validar los datos antes de guardarlos en la base de datos
